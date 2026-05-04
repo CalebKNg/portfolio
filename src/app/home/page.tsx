13cf3fb73bae93c8aa961cd0e56b3fb4fcd3a34e
CalebKNg <caleb17ng@gmail.com>
2026-05-03 22:43:01 -0700

    remove unecessary div

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,8 @@
 import Profile from "@/components/Profile"
 export default function Home() {
     return (
-        <div className="relative flex flex-col h-dvh">
-            <div className="flex-1 flex items-center justify-center">
-                <Profile/>
-            </div>
+        <div className="relative flex flex-col h-dvh items-center justify-center">
+            <Profile/>
         </div>
     )
 }
